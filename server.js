f064bfc59aa0e464faea08935a5cef031ed8f480
devrodriguez <john.rodriguez.25@hotmail.com>
2020-05-12 18:32:51 -0500

    Heroku deploy

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/multitienda-web'));
 app.listen(process.env.PORT || 8080);
 
 // Routing
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.hml'))
+    res.sendFile(path.join(__dirname + '/dist/multitienda-web/index.hml'))
 });
 
 console.log('Server listening...')
