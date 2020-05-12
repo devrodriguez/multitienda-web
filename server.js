bbcc1e7fe9ac8f39e722c2eeba88e1436c1c18fb
devrodriguez <john.rodriguez.25@hotmail.com>
2020-05-12 17:14:11 -0500

    Implements nodejs

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+// Routing
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.hml'))
+});
+
+console.log('Server listening...')
