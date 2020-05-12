130e84d2baf6d10f67defe9a8052f21dd086fd85
devrodriguez <john.rodriguez.25@hotmail.com>
2020-05-12 18:37:09 -0500

    Heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiGeoKey: 'AIzaSyBnh1nBPOMEml3EbvkzGn0c-LYEKMfKhfE',
   apiPlaceKey: 'AIzaSyA9UqT-ykyMUf2MJbc7EBsIQj6D69DHSJo',
-  apiHost: 'https://multitienda.herokuapp.com/'
+  apiHost: 'https://multitienda.herokuapp.com'
 };
